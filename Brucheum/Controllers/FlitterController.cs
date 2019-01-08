f77bd8eb340d8ef1ac173af7b7a70887aacd0b03
Curtis <curtishrhodes@hotmail.com>
2019-01-08 16:48:59 -0600

    waaa

---
+++ Brucheum/Controllers/FlitterController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Brucheum.Controllers
+{
+    public class FlitterController : Controller
+    {
+        private string apiService = System.Configuration.ConfigurationManager.AppSettings["apiService"];
+
+        public ActionResult Flitter()
+        {
+            ViewBag.UserId = User.Identity.GetUserId();
+            return View();
+        }
+        public ActionResult ToDo()
+        {
+            ViewBag.Service = apiService;
+            return View();
+        }
+    }
+}
