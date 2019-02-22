c57eecfccf1566efa90987bd1b481c82d7b27a36
Curtis <curtishrhodes@hotmail.com>
2019-02-21 18:11:27 -0600

    more ogglebooble changes

---
+++ OggleBooble/Scripts/Layouts.js
@@ -0,0 +1,43 @@
+﻿
+function setLayout(layoutName) {
+    //use "menuContainer" to remove logon tabs  #replaceableMenuItems to keep them
+    switch (layoutName) {
+
+        case 'porn':
+            $('#oggleBoobleHeader').removeClass('classicColors').addClass('pornColors');
+            $('.threeColumnArray').css("background-color", "#e2c2ab");
+            $('head title').html("girls sucking dick");
+            $('#divTopLeftLogo').html("<a href='/porn'><img src='/images/csLips02.png' class='bannerImage'/></a>");
+            $('#bannerTitle').html("The Church of My Cock");
+            $('#headerSubTitle').html("physical behavior catergorized");
+            break;
+        case 'mobileBoobs':
+            $('#divTopLeftLogo').html("<a href='/home'><img src='/images/redballon.png' class='littlelogo' /></a>");
+            $('head title').html("Mobile Boobs");
+            $('#bannerTitle').css("font-size", "15px");
+            $('#headerSubTitle').html(" ");
+            $('#headerMessage').html(" ");
+            $('#menuContainer').html(" ");
+            break;
+        case 'mobilePorn':
+            $('#oggleBoobleHeader').removeClass('classicColors').addClass('pornColors');
+            $('.threeColumnArray').css("background-color", "#e2c2ab");
+            $('#divTopLeftLogo').html("<a href='/porn'><img src='/images/csLips02.png' class='littlelogo' /></a>");
+            $('#bannerTitle').css("font-size", "15px").html("cocksuckers");
+            $('head title').html("Mobile Porn");
+            $('#headerSubTitle').html(" ");
+            $('#headerMessage').html(" ");
+            $('#menuContainer').html(" ");
+
+
+
+
+            //$('#headerSubTitle').html("physical behavior catergorized").css("font-size", "10px;");
+            //$('#menuContainer').html(`
+            //    <div class="menuTab floatLeft"><a href="/Home/Index/">CurtisRhodes.com</a></div>
+            //    <div class="menuTab floatLeft"><a href="/IntelDsgn/Index">Intelligent Design</a></div>
+            //    `);
+            break;
+        default:
+    }
+}
