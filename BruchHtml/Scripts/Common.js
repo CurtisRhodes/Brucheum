04bcd18964ceedb127e0cfacc35192f7ec622193
Curtis <curtishrhodes@hotmail.com>
2019-08-30 11:18:36 -0500

    introducing BruchHtml

---
+++ BruchHtml/Scripts/Common.js
@@ -0,0 +1,221 @@
+var settingsArray = {};
+var userRoles = [];
+
+$(document).ready(function () {
+    loadSettings();
+});
+
+function includeHTML() {
+    var z, i, elmnt, file, xhttp;
+    /* Loop through a collection of all HTML elements: */
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        /*search for elements with a certain atrribute:*/
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            /* Make an HTTP request using the attribute value as the file name: */
+            xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState === 4) {
+                    if (this.status === 200) { elmnt.innerHTML = this.responseText; }
+                    if (this.status === 404) { elmnt.innerHTML = "Page not found."; }
+                    /* Remove the attribute, and call this function once more: */
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            };
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            /* Exit the function: */
+            return;
+        }
+    }
+}
+function resizePage() {
+    // set page width
+    var winW = $(window).width();
+    var lcW = $('#leftColumn').width();
+    var rcW = $('#rightColumn').width();
+    $('#middleColumn').width(winW - lcW - rcW);
+
+    //set page height
+    var winH = $(window).height();
+    var headerH = $('header').height();
+    $('#middleColumn').height(winH - headerH - 155);
+}
+function getParams() {
+    var params = {},
+        pairs = document.URL.split('?').pop().split('&');
+    for (var i = 0, p; i < pairs.length; i++) {
+        p = pairs[i].split('=');
+        params[p[0]] = p[1];
+    }
+    return params;
+}
+function loadSettings() {
+    $.ajax({
+        type: "GET",
+        url: "Data/Settings.xml",
+        dataType: "xml",
+        success: function (xml) {
+            $(xml).find('setting').each(function () {
+                settingsArray[$(this).attr('name')] = $(this).attr('value');
+            });
+        },
+        error: function (jqXHR, exception) {
+            alert("getSettings jqXHR : " + getXHRErrorDetails(jqXHR, exception));
+        }
+    });
+}
+function displayStatusMessage(msgCode, message) {
+
+    var severityClassName;
+    switch (msgCode) {
+        case "ok":
+            severityClassName = "severityOk";
+            break;
+        case "warning":
+            severityClassName = "severityWarning";
+            break;
+        case "error":
+            severityClassName = "severityError";
+            break;
+        default:
+            severityClassName = msgCode;
+    }
+    //.severityOk {background - color: rgba(88, 139, 108, 0.75);    }
+    //.severityWarning {        background - color: #e6de3b;    }
+    //.severityError {        background - color: #c64e4e;    }
+
+    $('#divStatusMessage').removeClass();
+    $('#divStatusMessage').addClass(severityClassName);
+    $('#divStatusMessage').html(message);
+    $('#divStatusMessage').show();
+
+    if (msgCode === "ok") {
+        setTimeout(function () { $('#divStatusMessage').hide("slow"); }, 2500);
+    }
+    else {
+        setTimeout(function () { $('#divStatusMessage').hide("slow"); }, 15000);
+    }
+}
+
+function getXHRErrorDetails(jqXHR) {
+    var msg = '';
+    if (jqXHR.status === 0) {
+        msg = 'Not connect.\n Verify Network.';
+    } else if (jqXHR.status === 404) {
+        msg = 'Requested page not found. [404]';
+    } else if (jqXHR.status === 500) {
+        msg = 'Internal Server Error [500].';
+
+    } else if (jqXHR.responseText === 'parsererror') {
+        msg = 'Requested JSON parse failed.';
+    } else if (jqXHR.responseText === 'timeout') {
+        msg = 'Time out error.';
+    } else if (jqXHR.responseText === 'abort') {
+        msg = 'Ajax request aborted.';
+    } else {
+        msg = 'Uncaught Error.\n' + jqXHR.responseText;
+    }
+    return msg;
+}
+
+function isNullorUndefined(val) {
+    if (val === "")
+        return true;
+    if (val === null)
+        return true;
+    if (val === undefined)
+        return true;
+    return false;
+}
+
+function logActivity(changeLogModel) {
+    $.ajax({
+        type: "POST",
+        url: settingsArray.ApiServer + "/api/ChangeLog",
+        data: changeLogModel,
+        success: function (success) {
+            if (success === "ok")
+                displayStatusMessage("ok", "add image logged");
+            else
+                alert("ChangeLog: " + success);
+        },
+        error: function (xhr) {
+            $('#dashBoardLoadingGif').hide();
+            alert("ChangeLog xhr error: " + getXHRErrorDetails(xhr));
+        }
+    });
+}
+
+// HITCOUNTER
+function logVisit() {
+
+    //var x = getCookie("path");
+    //alert("cookie path: " + x);
+
+
+    //if ((ipAddress === "68.203.90.183") || (ipAddress === "50.62.160.105")) return "ok";
+    var logVisitUserName = getCookie("User");
+    //if (logVisitUserName !== "") {    }
+     //alert("logVisit UserName: " + logVisitUserName);
+
+    $('#footerMessage').html("logging visit userName: " + logVisitUserName);
+    setLoginHeader(logVisitUserName);
+    if (logVisitUserName === "") logVisitUserName = "unknown";
+    $.ajax({
+        type: "POST",
+        url: settingsArray.ApiServer + "api/HitCounter/LogVisit?userName=" + logVisitUserName + "&appName=Ogglebooble",
+        success: function (successModel) {
+            if (successModel.Success === "ok") {
+                $('#footerMessage').html("");
+                if (successModel.ReturnValue !== "") {
+                    $('#headerMessage').html(successModel.ReturnValue);
+                }
+            }
+            else
+                alert(successModel.Success);
+        },
+        error: function (jqXHR, exception) {
+            $('#blogLoadingGif').hide();
+            alert("LogVisit jqXHR : " + getXHRErrorDetails(jqXHR, exception));
+        }
+    });
+}
+function logPageHit(folderName, appName) {
+    //alert("logPageHit(" + folderName + "," + appName + ")");
+    logVisit();
+    $('#footerMessage').html("logging page hit");
+    var userName = getCookie("User");
+    if (userName === "")
+        userName = "unknown";
+    else {
+        setLoginHeader(userName);
+    }
+    //if ((ipAddress === "68.203.90.183") || (ipAddress === "50.62.160.105")) return "ok";
+    var hitCounterModel = {
+        AppId: appName,
+        PageName: folderName,
+        UserName: userName
+    };
+    $.ajax({
+        type: "PUT",
+        url: settingsArray.ApiServer + "api/HitCounter/LogPageHit",
+        data: hitCounterModel,
+        success: function (successModel) {
+            if (successModel.Success === "ok") {
+                if (userName === "unknown")
+                    $('#footerMessage').html("logPageHit: " + successModel.ReturnValue);
+                else
+                    $('#footerMessage').html("");
+            }
+            else
+                alert("logPageHit: " + successModel.Success);
+        },
+        error: function (jqXHR, exception) {
+            alert("logPageHit error: " + getXHRErrorDetails(jqXHR, exception));
+        }
+    });
+}
---
+++ BruchHtml/Scripts/Login.js
@@ -0,0 +1,292 @@
+﻿function onRegisterClick() {
+    $('#modalContainer').show();
+    $('#registerUserDialog').show();
+    $('#registerUserDialog').dialog({
+        show: { effect: "fade" },
+        hide: { effect: "blind" }
+    });
+    if (typeof pause === 'function')
+        pause();
+    $('#registerUserDialog').on('dialogclose', function (event) {
+        $('#modalContainer').hide();
+        $('#registerUserDialog').hide();
+        //$('#loginDialog').hide();
+        if (typeof resume === 'function')
+            resume();
+    });
+}
+
+function postRegister() {
+    if (validateRegister()) {
+        try {
+            var registeredUserModel = {};
+            registeredUserModel.UserName = $('#txtRegisterUserName').val();
+            registeredUserModel.Pswrd = $('#txtRegisterClearPassword').val();
+            registeredUserModel.FirstName = $('#txtFirstName').val();
+            registeredUserModel.LastName = $('#txtLastName').val();
+            registeredUserModel.Email = $('#ddCategory').val();
+            registeredUserModel.IpAddress = $('#txtPhone').val();
+            registeredUserModel.AppName = "OggleBoogle";
+
+            $.ajax({
+                type: "POST",
+                url: settingsArray.ApiServer + "/api/Login/RegisterUser",
+                data: registeredUserModel,
+                success: function (response) {
+                    if (response === "ok") {
+                        $('#registerUserDialog').dialog('close');
+                        setCookie($('#txtRegisterUserName').val());
+                        setLoginHeader($('#txtRegisterUserName').val());
+                    }
+                    else {
+                        $('#registerValidationSummary').html(response).show();
+                    }
+                },
+                error: function () {
+                    alert("Login Post failed");
+                }
+            });
+        } catch (e) {
+            alert("Login Post error: " + e);
+        }
+    }
+}
+
+function validateRegister() {
+    if ($('#txtRegisterUserName').val() === "") {
+        $('#errUserName').show();
+        return false;
+    }
+    $('#errUserName').hide();
+
+    if ($('#txtRegisterClearPassword').val() === "") {
+        $('#errRegisterPassword').show();
+        return false;
+    }
+
+    if ($('#txtRegisterClearPassword').val().length < 4) {
+        $('#errRegisterPassword').text("password must be at least 4 characters").show();
+        return false;
+    }
+    if ($('#txtRegisterClearPassword').val() !== $('#txtRegisterClearPasswordRetype').val()) {
+        $('#errRegisterPassword').text("password retype does not match").show();
+        return false;
+    }
+    $('#errRegisterPassword').hide();
+    return true;
+}
+
+function onLogoutClick() {
+
+    //alert("onLogoutClick: " + document.cookie);
+
+    //document.cookie = "OggleUser=; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "User=; path=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    document.cookie = "User=; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    //document.cookie = "expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    //document.cookie = null;
+
+    if (document.cookie)
+        alert("after logout document.cookie: " + document.cookie);
+
+    $('#optionLoggedIn').hide();
+    $('#optionNotLoggedIn').show();
+    $('.loginRequired').hide();
+}
+
+function onLoginClick() {
+    $('#modalContainer').show();
+    $('#loginDialog').show();
+    $('#loginDialog').dialog({
+        show: { effect: "fade" },
+        hide: { effect: "blind" },
+        width: 333
+    });
+    $('#loginDialog').show();
+    if (typeof pause === 'function')
+        pause();
+    $('#loginDialog').on('dialogclose', function (event) {
+        $('#modalContainer').hide();
+        $('#loginDialog').hide();
+        if (typeof resume === 'function')
+            resume();
+    });
+}
+
+function postLogin() {
+
+    if (validateLogin()) {
+        $.ajax({
+            type: "GET",
+            url: settingsArray.ApiServer + "api/Login/VerifyLogin?userName=" + $('#txtLoginUserName').val() + "&passWord=" + $('#txtLoginClearPassword').val(),
+            success: function (success) {
+                if (success === "ok") {
+                    $('#loginDialog').dialog('close');
+
+                    //alert("setCookie($('#txtLoginUserName').val()); " + $('#txtLoginUserName').val());
+
+                    setCookie($('#txtLoginUserName').val());
+
+                    displayStatusMessage("ok", "thanks for logging in " + getCookie("User"));
+
+                    setLoginHeader($('#txtLoginUserName').val());
+                    setUserPermissions();
+                }
+                else
+
+                    $('#loginValidationSummary').html(success).show();
+            },
+            error: function (jqXHR, exception) {
+                alert("validateLogin XHR error: " + settingsArray.ApiServer + "api/Login/VerifyLogin?userName" + $('#txtLoginUserName').val() + "&passWord=" + $('#txtLoginClearPassword').val() + "  " + getXHRErrorDetails(jqXHR, exception));
+            }
+        });
+    }
+    else {
+
+        alert("");
+    }
+}
+
+function validateLogin() {
+    if ($('#txtLoginUserName').val() === "") {
+        $('#errLoginUserName').show();
+        return false;
+    }
+    $('#errLoginUserName').hide();
+
+    if ($('#txtLoginClearPassword').val() === "") {
+        $('#errLoginPassword').show();
+        return false;
+    }
+    $('#errLoginPassword').hide();
+    return true;
+}
+
+function transferToRegisterPopup() {
+    $('#loginDialog').dialog('close');
+    onRegisterClick();
+}
+
+function profilePease() {
+    alert("profilePease");
+}
+
+function setUserPermissions() {
+    //alert("document.domain : " + document.domain);
+    if (document.domain === 'localhost') {
+        if (typeof permissionsSet === "boolean")
+            permissionsSet = true;
+        $('.loginRequired').show();
+        $('.adminLevelRequired').show();
+        if (typeof isPornEditor === 'boolean') 
+            isPornEditor = true;        
+        $('#spnUserName').html("devl");
+        $('#optionLoggedIn').hide();
+        $('#optionNotLoggedIn').hide();
+    }
+    else {
+
+        var userName = getCookie("User");
+        if (userName !== "") {
+            $.ajax({
+                type: "GET",
+                url: settingsArray.ApiServer + "api/Roles/GetUserRoles?userName=" + userName + "&whichType=Assigned",
+                success: function (roleModel) {
+                    if (roleModel.Success === "ok") {
+                        $.each(roleModel.RoleNames, function (idx, roleName) {
+                            //alert("roleModel.RoleName: " + roleName);
+                            if (roleName === "Oggle Add Images") {
+                                $('.loginRequired').show();
+                            }
+                            if (roleName === "Oggle admin") {
+                                $('.loginRequired').show();
+                                $('.adminLevelRequired').show();
+
+
+                                if (typeof isPornEditor === 'boolean') {
+                                    isPornEditor = true;
+                                    //alert("setUserPermissions isPornEditor: " + isPornEditor);
+                                    
+                                }
+                            }
+                        });
+                        if (typeof permissionsSet === "boolean") 
+                            permissionsSet = true;
+                    }
+                    else
+                        alert("loadUserRoles: " + roleModel.Success);
+                },
+                error: function (jqXHR, exception) {
+                    alert("loadUserRoles XHR error: " + getXHRErrorDetails(jqXHR, exception));
+                }
+            });
+        }
+        else
+            if (typeof permissionsSet === "boolean")
+                permissionsSet = true;
+
+    }
+}
+
+
+function isInRole(roleName) {
+    var userName = getCookie("User");
+    //alert("document.domain: " + document.domain);
+    //window.localStorage()
+    //if (userName !== "" || document.domain === 'localhost') {
+}
+
+// COOKIES
+function setLoginHeader(userName) {
+    if (userName === "") {
+        $('#optionLoggedIn').hide();
+        $('#optionNotLoggedIn').show();
+        $('.loginRequired').hide();
+    }
+    else {
+        $('#spnUserName').html(userName);
+        $('#optionLoggedIn').show();
+        $('#optionNotLoggedIn').hide();
+    }
+}
+
+function setCookie(userName) {
+    var expiryDate = new Date();
+    expiryDate.setMonth(expiryDate.getMonth() + 9);
+    document.cookie = 'User=' + userName + '; expires=' + expiryDate.toUTCString() + 'path=https://ogglebooble.com/';
+    alert("expires: " + expiryDate.toUTCString());
+}
+
+function getCookie(cname) {
+    var decodedCookie = "";
+    if (document.cookie) {
+        if (cname === undefined)
+            cname = "User";
+        var name = cname + "=";
+
+        if (cname !== "User")
+            alert("getCookie(cname): " + name);
+
+        decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                var cookieValue = c.substring(name.length, c.length);
+                //alert("cookie success: " + c.substring(name.length, c.length));
+
+                if (cname !== "User")
+                    alert("name: " + name + "  value" + cookieValue);
+
+
+                return cookieValue;
+            }
+        }
+    }
+    //else alert("no cookie found");
+    return decodedCookie;
+}
+
