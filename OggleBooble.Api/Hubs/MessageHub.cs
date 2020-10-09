444676e659f543aede643919e8db6c39e44a10f8
Curtis <curtishrhodes@hotmail.com>
2020-10-09 18:39:41 -0500

    added signalR this could be very dangerous

---
+++ OggleBooble.Api/Hubs/MessageHub.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+
+namespace OggleBooble.Api.Hubs
+{
+    public class MessageHub:Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("RecieveMessage", user, message);
+        }
+    }
+}
---
+++ OggleBooble.Web/Scripts/SendMessage.js
@@ -0,0 +1,40 @@
+﻿"use strict";
+
+let signalr = new signalR.HubConnectionBuilder().withUrl("/messageHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+signalr.on("ReceiveMessage", function (user, message) {
+    let msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    let encodedMsg = user + " says " + msg;
+    //var li = document.createElement("li");
+    //li.textContent = encodedMsg;
+    alert(encodedMsg);
+    //document.getElementById("messagesList").appendChild(li);
+});
+
+signalr.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    alert("signalR start");
+    console.log("signalR start");
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+function sendSignalR(message) {
+    signalr.invoke("SendMessage", "user", message).catch(function (err) {
+        alert("fff " + err.toString());
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+}
+
+//document.getElementById("sendButton").addEventListener("click", function (event) {
+//    var user = document.getElementById("userInput").value;
+//    var message = document.getElementById("messageInput").value;
+//    connection.invoke("SendMessage", user, message).catch(function (err) {
+//        return console.error(err.toString());
+//    });
+//    event.preventDefault();
+//});
