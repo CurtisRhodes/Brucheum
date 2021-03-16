44cc63b84c26616a3f4b68516fd0e873e0a03c84
Curtis Rhodes <curtishrhodes@hotmail.com>
2021-03-16 16:30:59 -0500

    me3

--- OggleBooble.Web/Scripts/IndexPage.js
+++ OggleBooble.Web/Scripts/IndexPage.js
@@ -1,47 +1,61 @@
-﻿let latestGallerySubdomain;
-function indexStartup() {
-    $('#indexMiddleColumn').html(indexPageHTML());
-    setOggleHeader(3908, "index");
-    setOggleFooter(3908, "index");
-    changeFavoriteIcon("redBallon");
-    document.title = "welcome : OggleBooble";
-    launchPromoMessages();
-    latestGallerySubdomain = "boobs";
-    launchCarousel("boobs");
-    $('.indexPageSection').show();
-    loadUpdatedGalleriesBoxes(updatedGalleriesCount, "boobs");
-}
+﻿let promoMessagesArray = new Array(),
+    promoIdx = 0;
+    promoMessageRotator = null,
+    promoMessageRotationSpeed = 15000,
+    numUpdatedGalleries = 25,
+    spaType = "archive";
 
-function pornStartup() {
-    $('#indexMiddleColumn').html(indexPageHTML());
-    setOggleHeader(spaPageId, "porn");
-    setOggleFooter(spaPageId, "porn");
-    changeFavoriteIcon("porn");
-    document.title = "OgglePorn";
-    latestGallerySubdomain = "porn";
-    launchCarousel("porn");
-    $('.threeColumnLayout').css("background-color", "#d279a6");
-    //if (subdomain == "porn")
-    //    $('.threeColumnLayout').css("background-color", "var(--oggleBackgroundColor)");
-    //else
-    //    $('.threeColumnLayout').css("background-color", "#d279a6");
 
-    $('#updatedGalleriesSectionLoadingGif').show();
-    loadUpdatedGalleriesBoxes(updatedGalleriesCount, "porn");
-    // set porn colors
-}
-
-function playboyStartup() {
-    $('#indexMiddleColumn').html(playboyPageHTML());
-    setOggleHeader(spaPageId, "playboyIndex");
-    setOggleFooter(spaPageId, "centerfold");
-    changeFavoriteIcon("centerfold");
-    document.title = "Every Playboy Centerfold";
-
-    latestGallerySubdomain = "centerfold";
-    launchCarousel("centerfold");
-    $('#updatedGalleriesSectionLoadingGif').show();
-    loadUpdatedGalleriesBoxes(updatedGalleriesCount, "centerfold");
+function displaySpaPage(spaPageId) {
+    switch (spaPageId) {
+        case 3907:
+            rankerStartup(params.bp);
+            break;
+        case 3911:
+            blogStartup();
+            break;
+        case 3910:
+            dashboardStartup();
+            break;
+        case 3908:  // boobs archive
+            //indexStartup();
+            $('#indexMiddleColumn').html(indexPageHTML());
+            setOggleHeader(3908, "index");
+            setOggleFooter(3908, "index");
+            changeFavoriteIcon("redBallon");
+            document.title = "welcome : OggleBooble";
+            launchCarousel("boobs");
+            $('.indexPageSection').show();
+            loadUpdatedGalleriesBoxes("boobs");
+            break;
+        case 3090: // porn
+            $('#indexMiddleColumn').html(indexPageHTML());
+            setOggleHeader(spaPageId, "porn");
+            setOggleFooter(spaPageId, "porn");
+            changeFavoriteIcon("porn");
+            document.title = "OgglePorn";
+            launchCarousel("porn");
+            // set porn colors
+            $('.threeColumnLayout').css("background-color", "#d279a6");
+            //if (subdomain == "porn")
+            //    $('.threeColumnLayout').css("background-color", "var(--oggleBackgroundColor)");
+            //else
+            //    $('.threeColumnLayout').css("background-color", "#d279a6");
+            $('#updatedGalleriesSectionLoadingGif').show();
+            loadUpdatedGalleriesBoxes("porn");
+            break;
+        case 72: // every playboy centerfold
+            $('#indexMiddleColumn').html(playboyPageHTML());
+            setOggleHeader(spaPageId, "playboyIndex");
+            setOggleFooter(spaPageId, "centerfold");
+            changeFavoriteIcon("centerfold");
+            document.title = "Every Playboy Centerfold";
+            launchCarousel("centerfold");
+            $('#updatedGalleriesSectionLoadingGif').show();
+            loadUpdatedGalleriesBoxes("centerfold");
+            break;
+        default:
+    }
 }
 
 function playboyPageHTML() {
@@ -65,7 +79,8 @@ function indexPageHTML() {
     return " <div class='indexPageSection' id='topIndexPageSection'>\n" +
         "       <div class='sectionLabel'>random galleries</div>\n" +
         "       <div id='testMessage1' class='indexPageHappyMessage' >start</div>\n" +
-        "           <div id='carouselContainer'></div>\n" +
+        "       <div id='promoContainer' class='promoContainer' >my promo message</div>\n" +
+        "       <div id='carouselContainer'></div>\n" +
         "    </div>\n" +
         //"    <div class='clickable sectionLabel' onclick='cureWIPproblem(211,\"018d1162-61a6-4987-bd90-add6fac518c6\",\"WIP\")'>cure WIP problem</div>\n" +
         //"    <div class='clickable sectionLabel' onclick='myMsgTest()'>showMyAlert test</div>\n" +
@@ -81,13 +96,13 @@ function indexPageHTML() {
         "       onclick='showMoreGalleries()'>show more updated galleries</div>\n";
 }
 
-function loadUpdatedGalleriesBoxes(numItmes) {
+function loadUpdatedGalleriesBoxes(spaType) {
     let settingsImgRepo = settingsArray.ImageRepo, thisItemSrc;
     $('#updatedGalleriesSectionLoadingGif').show();
     let getLatestStart = Date.now();
     $.ajax({
         type: "GET",
-        url: settingsArray.ApiServer + "api/LatestUpdates/GetLatestUpdatedFolders?take=" + numItmes + "&root=" + latestGallerySubdomain,
+        url: settingsArray.ApiServer + "api/LatestUpdates/GetLatestUpdatedFolders?take=" + numUpdatedGalleries + "&root=" + spaType,
         success: function (latestUpdates) {
             if (latestUpdates.Success === "ok") {
                 //$('#updatedGalleriesSectionLoadingGif').hide();
@@ -104,7 +119,7 @@ function loadUpdatedGalleriesBoxes(numItmes) {
                             "<div class='newsContentBoxLabel'>" + LatestUpdate.FolderName + "</div>" +
                             "<img id='lt" + LatestUpdate.FolderId + "' class='newsContentBoxImage' " +
                             "onerror='latestGalleryImageError(" + LatestUpdate.FolderId + ",\"" + thisItemSrc + "\")' src='" + thisItemSrc + "'" +
-                            "onclick='rtpe(\"LUP\",\"" + latestGallerySubdomain + "\",\"" + LatestUpdate.FolderName + "\"," + LatestUpdate.FolderId + ")' />" +
+                            "onclick='rtpe(\"LUP\",\"" + spaType + "\",\"" + LatestUpdate.FolderName + "\"," + LatestUpdate.FolderId + ")' />" +
                             "<div class='newsContentBoxDateLabel'>updated: " + dateString2(LatestUpdate.Acquired) + "</span></div>" +
                             "</div>");
                     }
@@ -112,6 +127,9 @@ function loadUpdatedGalleriesBoxes(numItmes) {
                 //$('.sectionLabel').show();
                 resizeIndexPage();
                 setTimeout(function () { resizeIndexPage(); }, 300);
+
+                setTimeout(function () { launchPromoMessages(); }, 3000);
+
                 var delta = (Date.now() - getLatestStart) / 1000;
                 console.log("loaded " + latestUpdates.LatestTouchedGalleries.length + " news boxes.  Took: " + delta.toFixed(3));
             }
@@ -120,7 +138,7 @@ function loadUpdatedGalleriesBoxes(numItmes) {
         error: function (jqXHR) {
             let errMsg = getXHRErrorDetails(jqXHR);
             let functionName = arguments.callee.toString().match(/function ([^\(]+)/)[1];
-            if (!checkFor404(errMsg, folderId, functionName)) logError("XHR", latestGallerySubdomain, errMsg, functionName);
+            if (!checkFor404(errMsg, folderId, functionName)) logError("XHR", subdomain, errMsg, functionName);
         }
     });
 }
@@ -148,13 +166,13 @@ function launchPromoMessages() {
             if (blogCommentsContainer.Success === "ok") {
                 $.each(blogCommentsContainer.BlogComments, function (idx, blogComment) {
                     promoMessagesArray.push({
-                        FolderId: blogComment.Id,
-                        Link: blogComment.Link,
+                        //FolderId: blogComment.FolderId,
+                        //Link: blogComment.ImageLink,
                         CommentTitle: blogComment.CommentTitle,
                         CommentText: blogComment.CommentText
                     });
                 });
-                showPromoMessages();
+                showPromoMessages(promoMessagesArray);
             }
             else {
                 alert("launchPromoMessages: " + blogCommentsContainer.Success);
@@ -165,25 +183,31 @@ function launchPromoMessages() {
             $('#indexPageLoadingGif').hide();
             let errMsg = getXHRErrorDetails(jqXHR);
             let functionName = arguments.callee.toString().match(/function ([^\(]+)/)[1];
-            if (!checkFor404(errMsg, folderId, functionName)) logError("XHR", latestGallerySubdomain, errMsg, functionName);
+            if (!checkFor404(errMsg, folderId, functionName)) logError("XHR",3908, errMsg, functionName);
         }
     });
 }
 
-function showPromoMessages() {
-    $('#promoContainer').fadeIn();
-    var currentItem = promoMessagesArray[promoIdx];
-    $('#promoContainerTitle').html(currentItem.CommentTitle);
-    $('#promoContainerText').html(currentItem.CommentText);
+function showPromoMessages(promoMessagesArray) {
+    $('#promoContainer').html(
+    "<div class='promoFlexContainer'>\n" +
+        "<div id='promoContainerTitle' class='ogglePromoTitle'></div>\n" +
+        "<div id='promoContainerText' class='ogglePromoText'></div>\n" +
+        "<div onclick='killPromoMessages()' class='tinyDots' onmouseover='$(\"#killPromoPrompt\").show()' onmouseout='$(\"#killPromoPrompt\").hide()'>...</div>\n" +
+        "<div id='killPromoPrompt' class='ogglePromoKillMessage'>had enough promo messages?</div>\n" +
+    "</div>");
+    $('#promoContainer').fadeIn(800,"");
+    $('#promoContainer').draggable().resizable();
+    //$('#promoContainerText').resizable();
+    //alert("promoMessagesArray[promoIdx].CommentTitle: " + promoMessagesArray[promoIdx].CommentTitle);
+    $('#promoContainerTitle').html(promoMessagesArray[promoIdx].CommentTitle);
+    $('#promoContainerText').html(promoMessagesArray[promoIdx].CommentText);
 
     promoMessageRotator = setInterval(function () {
-
-        if (promoIdx === promoMessagesArray.length) {
-            promoIdx = 0;
-        }
+        if (promoIdx === promoMessagesArray.length) promoIdx = 0;        
         currentItem = promoMessagesArray[promoIdx];
-        $('#promoContainerTitle').html(currentItem.CommentTitle);
-        $('#promoContainerText').html(currentItem.CommentText);
+        $('#promoContainerTitle').html(promoMessagesArray[promoIdx].CommentTitle);
+        $('#promoContainerText').html(promoMessagesArray[promoIdx].CommentText);
         promoIdx++;
     }, promoMessageRotationSpeed);
 }
@@ -195,7 +219,7 @@ function killPromoMessages() {
 }
 
 function showMoreGalleries() {
-    updatedGalleriesCount += 15;
+    latestGalleriesCount += 15;
     loadUpdatedGalleriesBoxes(updatedGalleriesCount);
 }
 
@@ -205,15 +229,6 @@ function showHideGalleries() {
     resizeIndexPage();
 }
 
-function showPromoMessagesHtml() {
-    $('#promoMessagesContainer').html(
-        "<div id='promoContainer' class='ogglePromoContainer'>\n" +
-        "    <div id='promoContainerTitle' class='ogglePromoTitle'></div>\n" +
-        "    <div id='promoContainerText' class='ogglePromoText'></div>\n" +
-        "    <div onclick='killPromoMessages()' class='tinyDots' onmouseover='$('#killPromoPrompt').show()' onmouseout='$('#killPromoPrompt').hide()'>...</div>\n" +
-        "    <div id='killPromoPrompt' class='ogglePromoKillMessage'>had enough promo messages?</div>\n" +
-        "</div>\n");
-}
 
 function goToPorn() {
 
