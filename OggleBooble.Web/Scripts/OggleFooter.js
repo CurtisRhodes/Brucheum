c862b47b62c7b3b0a1daf8d4cd0cd917fbcf4562
Curtis <curtishrhodes@hotmail.com>
2021-04-19 04:19:33 -0500

    oh boy

--- OggleBooble.Web/Scripts/OggleFooter.js
+++ OggleBooble.Web/Scripts/OggleFooter.js
@@ -87,6 +87,7 @@
         case "playboy":
         case "cybergirl":
         case "muses": 
+        case "plus": 
         case "magazine": 
         case "centerfold": {
             $('#footerCol1').html(
@@ -127,20 +128,20 @@
                 "<div class='clickable' onclick='rtpe(\"FLC\",\"rejects\",\"rejects\"," + folderId + ")'>Rejects</div>\n" +
                 "<div class='clickable' onclick='rtpe(\"FLC\",\"videos\",\"videos\"," + folderId + ")'>Videos</div>\n");
 
-            //$('#footerCol6').html(`<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
-            //    <!-- Histats.com  START  (aync)-->
-            //    <script type="text/javascript">var _Hasync= _Hasync|| [];
-            //    _Hasync.push(['Histats.start', '1,4458214,4,30,130,80,00010101']);
-            //    _Hasync.push(['Histats.fasi', '1']);
-            //    _Hasync.push(['Histats.track_hits', '']);
-            //    (function() {
-            //    var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-            //    hs.src = ('//s10.histats.com/js15_as.js');
-            //    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-            //    })();</script>
-            //    <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4458214&101" alt="page hit counter" border="0"></a></noscript>
-            //    <!-- Histats.com  END  -->`
-            //);
+            $('#footerCol6').html(`<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+                <!-- Histats.com  START  (aync)-->
+                <script type="text/javascript">var _Hasync= _Hasync|| [];
+                _Hasync.push(['Histats.start', '1,4458214,4,30,130,80,00010101']);
+                _Hasync.push(['Histats.fasi', '1']);
+                _Hasync.push(['Histats.track_hits', '']);
+                (function() {
+                var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+                hs.src = ('//s10.histats.com/js15_as.js');
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+                })();</script>
+                <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4458214&101" alt="page hit counter" border="0"></a></noscript>
+                <!-- Histats.com  END  -->`
+            );
             break;
         }
         default: {
